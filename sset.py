b600aa42e9f36df344c330a07b4606b57d7c4d09
Nick Travers <n.e.travers@gmail.com>
2013-02-16 14:58:17 +0800

    Working SSET

---
+++ sset.py
@@ -0,0 +1,21 @@
+import itertools, math
+
+def combinations(n, k):
+	return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+def main():
+
+	# import the string from file
+	f = open('/Volumes/Data/nick/Downloads/rosalind_sset.txt', 'r')
+
+	n = int(f.readline().split('\n')[0])
+	f.close()
+
+	count = 1
+	for i in range(1,n+1):
+		count += combinations(n, i)
+
+	print count % 1000000
+
+if __name__ == '__main__':
+	main()
