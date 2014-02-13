76bf67f52d6b43f6cb918dcad7222aad5db89be5
Nick Travers <n.e.travers@gmail.com>
2014-02-13 22:07:15 +0800

    Add catalan number function

--- cat.py
+++ cat.py
@@ -1,3 +1,20 @@
+CAT_LIST = {0:1, 1:1}
+def catalan(n):
+	"""Return the n-th catalan number"""
+	
+	# if the number is already in the dictionary, return it
+	if n in CAT_LIST:
+		return CAT_LIST[n]
+
+	# otherwise, recursively compute it
+	s = 0
+	for k in range(1, n+1):
+		s += catalan(k - 1) * catalan(n - k)
+
+	CAT_LIST[n] = s
+	return CAT_LIST[n]
+
+
 def main():
 	# import file for reading
 	f = open('/Users/nick/Downloads/rosalind_cat.txt', 'r')
