7469543318ab5236cb3faf305a856d6adb48ef0f
Nick Travers <n.e.travers@gmail.com>
2014-03-30 14:29:55 +0800

    Complete pcov
    - return only one simple cycle, corresponding to the supersting
    of minimal length

--- pcov.py
+++ pcov.py
@@ -1,3 +1,5 @@
+import networkx as nx
+
 class DNAString(object):
 	"""DNA String class"""
 	def __init__(self, string):
@@ -45,30 +47,63 @@ class DNAString(object):
 	def suffix(self):
 		return self.string[1:]	
 
+def addNodes(G, l):
+	"""Add elements of the list l as nodes to the graph, G"""
+	G.add_nodes_from(l)
+
+	return G
+
+def addEdges(G, l, S):
+	"""Add edges from the list l, to the graph G"""
+	for kmer in l:
+		prefix = kmer.prefix()
+		suffix = kmer.suffix()
+
+		if (prefix in G.nodes()) and (suffix in G.nodes()) and (kmer in S):
+			G.add_edge(prefix, suffix, string=kmer)
+		else:
+			continue
+
 def main():
+
 	# open the file of strings
 	with open('/Users/nick/Downloads/rosalind_pcov.txt', 'r') as f:
-		lines = f.read().split('\n')
+		lines = f.read().split('\n')[:-1]
 
 	# instantiate the DNAStrings
 	S = []
+	Src = []
 	for s in lines:
 		S.append(DNAString(s))
+		Src.append(DNAString(s).reverseComplement())
 
-	# the reverse complement
-	Src = S
-	for s in Src:
-		s.reverseComplement()
+	SuSrc = list(set(S).union(Src))
 
-	both = list(set(S) | set(Src))
-	print both
-	 
-	o = []
-	for i in both:
-		o.append(i.prefix())
-		o.append(i.suffix())
+	prefixSuffix = []
+	for i in SuSrc:
+		prefixSuffix.append(i.prefix())
+		prefixSuffix.append(i.suffix())
 
-	print list(set(o))
+	prefixSuffix = list(set(prefixSuffix))
+
+	# instantiate the graph
+	G = nx.DiGraph()
+
+	# add nodes
+	addNodes(G, prefixSuffix)
+
+	# add edges
+	addEdges(G, SuSrc, S)
+
+	# the simple cycles of the graph
+	cycles = list(nx.simple_cycles(G))
+
+	# if there is more than one simple cycle, raise error
+	if len(cycles) > 1:
+		print 'More than one simple cycle!?'
+		print cycles
+	else:
+		print ''.join(map(lambda x: x[-1], cycles[0]))
 
 
 if __name__ == '__main__':
